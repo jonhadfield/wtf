fa8d4421f11640521c9c90bdcbbc8da291546a79
Jon Hadfield <jon@lessknown.co.uk>
2020-04-25 19:33:50 +0100

    add ping module.

---
+++ modules/ping/check.go
@@ -0,0 +1,261 @@
+package ping
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+
+	"github.com/wtfutil/wtf/logger"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+)
+
+const (
+	ProtocolICMP     = 1
+	ProtocolIPv6ICMP = 58
+)
+
+// https://github.com/rogpeppe-contrib/golang-net/blob/master/icmp/ping_test.go
+
+func checkIPV4(target string, pingTimeout int) (result string) {
+	dst, err := net.ResolveIPAddr("ip4", target)
+	if err != nil {
+		logger.Log(fmt.Sprintf("failed to resolve %s", target))
+		return "fail"
+	}
+
+	var conn *icmp.PacketConn
+
+	conn, err = icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		logger.Log("failed to listen for ip4:icmp packets")
+		return "fail"
+	}
+
+	m := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1 << uint(rand.Uint32()),
+			Data: []byte(""),
+		},
+	}
+
+	var b []byte
+
+	b, err = m.Marshal(nil)
+	if err != nil {
+		return "fail"
+	}
+
+	var n int
+
+	logger.Log(fmt.Sprintf("pinging: %s", dst.String()))
+
+	n, err = conn.WriteTo(b, dst)
+	if err != nil {
+		logger.Log(fmt.Sprintf("failed to send ping to %s", dst.String()))
+		return "fail"
+	} else if n != len(b) {
+		logger.Log(fmt.Sprintf("failed to send ping to %s", dst.String()))
+		return "fail"
+	}
+
+	reply := make([]byte, 1500)
+
+	for {
+		waitStart := time.Now()
+
+		err = conn.SetReadDeadline(time.Now().Add(time.Duration(pingTimeout) * time.Second))
+		if err != nil {
+			logger.Log(fmt.Sprintf("failed to set response timeout for %s", dst.String()))
+			return "fail"
+		}
+
+		var peer net.Addr
+
+		n, peer, err = conn.ReadFrom(reply)
+		if err != nil {
+			logger.Log(fmt.Sprintf("failed to read reply for target: %s %v", target, err))
+			return "fail"
+		}
+
+		if dst.String() != peer.String() {
+			logger.Log(fmt.Sprintf("received reply for %s from wrong peer: %s. continue waiting",
+				dst.String(), peer.String()))
+
+			waitEnd := time.Since(waitStart)
+
+			pingTimeout -= int(math.Round(waitEnd.Seconds()))
+
+			continue
+		}
+
+		logger.Log(fmt.Sprintf("got reply for %s", dst.String()))
+
+		break
+	}
+
+	var rm *icmp.Message
+
+	rm, err = icmp.ParseMessage(ProtocolICMP, reply[:n])
+	if err != nil {
+		return "fail"
+	}
+
+	if rm.Type == ipv4.ICMPTypeEchoReply {
+		return "success"
+	}
+
+	return "fail"
+}
+
+func checkIPV6(target string, pingTimeout int) (result string) {
+	dst, err := net.ResolveIPAddr("ip6", target)
+	if err != nil {
+		logger.Log(fmt.Sprintf("failed to resolve %s", target))
+		return "fail"
+	}
+
+	var conn6 *icmp.PacketConn
+
+	conn6, err = icmp.ListenPacket("ip6:ipv6-icmp", "::")
+	if err != nil {
+		logger.Log("failed to listen for ip6:ipv6-icmp packets")
+		return "fail"
+	}
+
+	m := icmp.Message{
+		Type: ipv6.ICMPTypeEchoRequest, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1,
+			Data: []byte(""),
+		},
+	}
+
+	var b []byte
+
+	b, err = m.Marshal(nil)
+	if err != nil {
+		return "fail"
+	}
+
+	var n int
+
+	logger.Log(fmt.Sprintf("pinging: %s", dst.String()))
+
+	n, err = conn6.WriteTo(b, dst)
+	if err != nil {
+		logger.Log(fmt.Sprintf("failed to send ping to %s", dst.String()))
+		return "fail"
+	} else if n != len(b) {
+		logger.Log(fmt.Sprintf("failed to send ping to %s", dst.String()))
+		return "fail"
+	}
+
+	reply := make([]byte, 1500)
+
+	for {
+		waitStart := time.Now()
+
+		err = conn6.SetReadDeadline(time.Now().Add(time.Duration(pingTimeout) * time.Second))
+		if err != nil {
+			return "fail"
+		}
+
+		var peer net.Addr
+
+		n, peer, err = conn6.ReadFrom(reply)
+		if err != nil {
+			logger.Log(fmt.Sprintf("failed to read reply for target: %s %v", target, err))
+			return "fail"
+		}
+
+		if dst.String() != peer.String() {
+			logger.Log(fmt.Sprintf("received reply for %s from wrong peer: %s. continue waiting.", dst.String(), peer.String()))
+
+			waitEnd := time.Since(waitStart)
+			pingTimeout -= int(math.Round(waitEnd.Seconds()))
+
+			continue
+		}
+
+		break
+	}
+
+	var rm *icmp.Message
+
+	rm, err = icmp.ParseMessage(ProtocolIPv6ICMP, reply[:n])
+	if err != nil {
+		return "fail"
+	}
+
+	if rm.Type == ipv6.ICMPTypeEchoReply {
+		return "success"
+	}
+
+	return "fail"
+}
+
+func checkTarget(t *net.IP, pingTimeout int) (result string) {
+	if t.To4() != nil {
+		result = checkIPV4(t.String(), pingTimeout)
+		return
+	}
+
+	result = checkIPV6(t.String(), pingTimeout)
+
+	return
+}
+
+func ipsFromTarget(t string) (ips []*net.IP, isIP bool, err error) {
+	// try to parse target as an IP
+	if i := net.ParseIP(t); i != nil {
+		// return the target parsed as an IP
+		return []*net.IP{&i}, true, nil
+	}
+
+	// try to parse target as an FQDN
+	var pIPs []string
+
+	pIPs, err = net.LookupHost(t)
+	if err != nil {
+		logger.Log(fmt.Sprintf("lookup failed for: %s", t))
+		return
+	}
+
+	// parse each IP
+	for x := 0; x < len(pIPs); x++ {
+		pIP := net.ParseIP(pIPs[x])
+		ips = append(ips, &pIP)
+	}
+
+	return
+}
+
+type target struct {
+	raw string
+	ips []*net.IP
+	err error
+}
+
+func parseTargets(ts []string) (targets []target) {
+	for _, t := range ts {
+		res, isIP, err := ipsFromTarget(t)
+		// if parsing shortens ipv6 address then use shorter version for checking and display
+		if isIP && res[0].String() != t {
+			t = res[0].String()
+		}
+
+		targets = append(targets, target{
+			raw: t,
+			ips: res,
+			err: err,
+		})
+	}
+
+	return
+}
---
+++ modules/ping/settings.go
@@ -0,0 +1,33 @@
+package ping
+
+import (
+	"github.com/olebedev/config"
+	"github.com/wtfutil/wtf/cfg"
+	"github.com/wtfutil/wtf/utils"
+)
+
+const (
+	defaultFocusable = true
+	defaultTitle     = "PingCheck"
+)
+
+type Settings struct {
+	common      *cfg.Common
+	targets     []string
+	pingTimeout int
+	showIP      bool
+	format      bool
+	formatStyle string
+	wrapText    bool
+}
+
+func NewSettingsFromYAML(name string, ymlConfig *config.Config, globalConfig *config.Config) *Settings {
+	settings := Settings{
+		common:      cfg.NewCommonSettingsFromModule(name, defaultTitle, defaultFocusable, ymlConfig, globalConfig),
+		targets:     utils.ToStrs(ymlConfig.UList("targets")),
+		showIP:      ymlConfig.UBool("showIP", true),
+		pingTimeout: ymlConfig.UInt("pingTimeout", 4),
+	}
+
+	return &settings
+}
